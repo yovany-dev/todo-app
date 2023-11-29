e42bd4ec57180e752bdb4a3a7cda702ba5b0056e
Yovani Morales <83714102+Yovani-Morales@users.noreply.github.com>
2023-11-29 14:35:20 -0600

    feat(modal): add and cancel

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
+import { TodoContext } from './TodoContext/TodoContext';
 import '../style/form.css';
 
 const TodoForm = () => {
+  const {addTodo, setOpenModal} = React.useContext(TodoContext);
+  const [newTodoValue, setNewTodoValue] = React.useState('');
+
+  const onSubmit = e => {
+    e.preventDefault();
+    addTodo(newTodoValue);
+    setOpenModal(false);
+  }
+
+  const onCancel = () => {
+    setOpenModal(false);
+  }
+
+  const onChange = e => {
+    setNewTodoValue(e.target.value);
+  }
+
   return (
-    <form className='form' onSubmit={e => e.preventDefault()}>
+    <form className='form' onSubmit={onSubmit}>
       <h2>Escribe tu <br /> nuevo TODO</h2>
-        <textarea placeholder="Desarrollar componentes de React"></textarea>
+        <textarea value={newTodoValue} onChange={onChange} placeholder="Desarrollar componentes de React"></textarea>
         <div className="btn-container">
-          <button className="btn close">Cancelar</button>
-          <button className="btn add">Añadir</button>
+          <button type='button' onClick={onCancel} className="btn close">Cancelar</button>
+          <button type='submit' className="btn add">Añadir</button>
         </div>
     </form>
   )
